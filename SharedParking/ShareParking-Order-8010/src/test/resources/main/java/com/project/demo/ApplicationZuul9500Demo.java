d160e9242e5044085674cb212fe2e2e0e85117bc
zhangbo314159 <zhangbo_ss@126.com>
2019-07-20 20:31:47 +0800

    order

---
+++ SharedParking/ShareParking-Order-8010/src/test/resources/main/java/com/project/demo/ApplicationZuul9500Demo.java
@@ -0,0 +1,12 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationZuul9500Demo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationZuul9500Demo.class, args);
+	}
+}
