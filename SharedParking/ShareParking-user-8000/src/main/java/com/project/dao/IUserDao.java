0967d14f7b9886588d43d87989e0fd5358732e34
zhouchengzhuang <97674657@qq.com>
2019-07-22 17:13:19 +0800

    解决智障的bug1

--- SharedParking/ShareParking-user-8000/src/main/java/com/project/dao/IUserDao.java
+++ SharedParking/ShareParking-user-8000/src/main/java/com/project/dao/IUserDao.java
@@ -1,7 +1,34 @@
 package com.project.dao;
 
 
+import com.project.Bean.UserBean;
+import org.apache.ibatis.annotations.Update;
+
+/*
+    管理员 dao接口
+ */
 public interface IUserDao {
+    /*
+        通过用户名查找
+     */
+    UserBean findByUserName(String username);
+
+    /*、
+        通过id查询
+     */
+    UserBean findById(int id);
+
+    /*
+        修改手机号
+     */
+    @Update("update user set phonenumber = #{phone} where id=#{id}")
+    int updatePhoneNumber(String phone,int id);
+
+    /*
+        修改密码
+     */
+    @Update("update user set password=#{repassword} where id = #{id}")
+    int updatePassword(String repassword,int id);
 
 
 }
