2d03a0d3253e1431e74d760f02766b41e7cbc2d3
zhaoxuechuan <2238421926@qq.com>
2019-07-22 19:45:36 +0800

    fdagfg

--- SharedParking/ShareParking_carport_8050/src/main/java/com/project/controller/CarportController.java
+++ SharedParking/ShareParking_carport_8050/src/main/java/com/project/controller/CarportController.java
@@ -3,6 +3,7 @@ package com.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.project.Bean.CarportBean;
 import com.project.IService.CarportIService;
@@ -12,6 +13,7 @@ public class CarportController {
 	@Autowired
 private CarportIService service;
 	@GetMapping(value="/carport")
+	@ResponseBody
 public CarportBean findcarportbymid(int mid) {
 		System.out.println("000000");
 	CarportBean carport =service.findcarportbymid(mid);
