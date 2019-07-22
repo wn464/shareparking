1cb4e5e369807d5b5d4d12257f1c09079c528342
xiaolipk <1120091242@qq.com>
2019-07-22 15:20:04 +0800

    .

---
+++ SharedParking/ShareParking-comment/src/main/java/com/project/Bean/ComplaintBean.java
@@ -0,0 +1,20 @@
+package com.project.Bean;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)//链式表达
+public class ComplaintBean {
+	private int id;
+	private String message;
+	private String Comtime;
+	private String imgName;
+	private int o_id;
+	private int mem_j_id;
+	private int mem_y_id;
+
+}
