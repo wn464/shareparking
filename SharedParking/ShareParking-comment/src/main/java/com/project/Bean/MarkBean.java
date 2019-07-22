baeabeacfd3be35d6688e490999761465029f511
zhangbo314159 <zhangbo_ss@126.com>
2019-07-22 12:07:08 +0800

    orderbean

---
+++ SharedParking/ShareParking-comment/src/main/java/com/project/Bean/MarkBean.java
@@ -0,0 +1,12 @@
+package com.project.Bean;
+
+import lombok.Data;
+
+@Data
+public class MarkBean {
+
+	private int id;
+	private String name;
+	private String message;
+	
+}
---
+++ SharedParking/ShareParking-comment/src/main/java/com/project/Bean/OrderBean.java
@@ -0,0 +1,19 @@
+package com.project.Bean;
+
+import java.util.List;
+
+
+
+public class OrderBean {
+
+	private int id;//订单主键
+	private String orderTime;//订单生成时间
+	private String orderNumber;//订单号
+	private String alipayNumber;//支付宝号
+	private double price;//订单金额
+	private int delState;//删除状态
+	private String carNumber;
+	private String beginTime;
+	private String endTime;
+	
+}
