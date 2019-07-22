8c2b3dc7ea740662bc1b1682b29e86b8e5edb95f
zhaoxuechuan <2238421926@qq.com>
2019-07-22 16:02:14 +0800

    配置

---
+++ SharedParking/ShareParking_carport_8050/src/main/java/com/project/dao/CarportDao.java
@@ -0,0 +1,68 @@
+package com.project.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.project.Bean.CarportBean;
+
+
+
+
+
+/**
+ * 停车位接口
+ * @author zxc
+ *
+ */
+public interface CarportDao {
+	/**
+	 * 通过车位所属人查询车位
+	 * @param mid
+	 * @return
+	 */
+	CarportBean findcarportbyMID(@Param("mid")int mid);
+	/**
+	 * 通过坐标查询坐标范围内的车位
+	 * @param coordinate_x
+	 * @param coordinate_y
+	 * @return
+	 */
+	List<CarportBean> findcarportbycoordinate(@Param("x")double coordinate_x,@Param("y")double coordinate_y,@Param("page")int page,@Param("size")int size);
+	/**
+	 * 通过出租车为时间查找时间内的车位
+	 * @param begintime
+	 * @param endtime
+	 * @return
+	 */
+	List<CarportBean> findcarportbytime(@Param("btime")Date begintime,@Param("etime")Date endtime,@Param("page")int page,@Param("size")int size);
+	/**
+	 * 通过地址来查找车位
+	 * @param address
+	 * @return
+	 */
+	List<CarportBean> findcarportbyaddress(@Param("address")String address,@Param("page")int page,@Param("size")int size);
+	/**
+	 * 通过车位状态来查询车位
+	 * @param status
+	 * @param page
+	 * @param size
+	 * @return
+	 */
+	List<CarportBean> findcarportbyStatus(@Param("status")int status,@Param("page")int page,@Param("size")int size);
+	/**
+	 * 通过id修改车位的状态
+	 * @param id
+	 * @param status
+	 * @return
+	 */
+	int updatecarport(int id,int status);
+	/**
+	 * 通过价格范围查询车位
+	 * @param price
+	 * @return
+	 */
+	List<CarportBean> findcarportbyprice(@Param("minprice")double minprice,@Param("maxprice")double maxprice);
+	
+}
---
+++ SharedParking/ShareParking_carport_8050/src/main/java/com/project/demo/Application8050demo.java
@@ -0,0 +1,11 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application8050demo {
+public static void main(String[] args) {
+	SpringApplication.run(Application8050demo.class, args);
+}
+}
