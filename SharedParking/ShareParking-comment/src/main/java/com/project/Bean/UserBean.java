d801e5dc06e6212295db15d874828e415a7e3217
zhouchengzhuang <97674657@qq.com>
2019-07-23 17:08:43 +0800

    滴滴滴滴滴滴

--- SharedParking/ShareParking-comment/src/main/java/com/project/Bean/UserBean.java
+++ SharedParking/ShareParking-comment/src/main/java/com/project/Bean/UserBean.java
@@ -1,11 +1,9 @@
 package com.project.Bean;
 
-import lombok.Data;
-
 /*
     后台用户bean
  */
-@Data
+
 public class UserBean {
     private int id;
     private String username;
@@ -17,5 +15,75 @@ public class UserBean {
     private String email;
     private AuthorityBean authority;        //权限
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public AuthorityBean getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(AuthorityBean authority) {
+        this.authority = authority;
+    }
 }
---
+++ SharedParking/ShareParking-user-8001/src/main/java/com/project/controller/UserHandler.java
@@ -0,0 +1,92 @@
+package com.project.controller;
+
+import com.project.Bean.UserBean;
+import com.project.service.IUserService;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/*
+    管理员controller
+ */
+@RestController
+public class UserHandler {
+    @Autowired
+    private IUserService service;
+
+
+    /*
+        ------ 登录 模块 -----
+        0 : 前台输入的格式不正确
+        2 ：密码输入错误
+        1 ：登录成功
+     */
+    @GetMapping("/user/login")
+    public int login(@Validated UserBean user, BindingResult result){
+        if (result.hasErrors()){
+            System.out.println("------------出现错误---------");
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            for (FieldError fieldError : fieldErrors) {
+                System.out.println(fieldError.getDefaultMessage());
+            }
+            return 0;
+        }else{
+            Subject subject = SecurityUtils.getSubject();
+            UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(),user.getPassword());
+            if (!subject.isAuthenticated()){
+                try {
+                    subject.login(token);
+                    System.out.println("------认证成功------");
+
+                    //把id存到session
+                    UserBean bean = service.findByUserName(user.getUsername());
+                    Session session = subject.getSession();
+                    session.setAttribute("id",bean.getId());
+
+                    return 1;
+                }catch (Exception e){
+                    System.out.println("------认证失败------");
+                    return 2;
+                }
+            }
+        }
+        return 1;
+    }
+
+    /*
+        ------------------------查询模块----------------------
+     */
+    //查询所有管理员
+    @GetMapping("/user/{id}")
+    public List<UserBean> findAll(Integer id){
+        List<UserBean> list = service.findAll(id);
+        return list;
+    }
+
+
+    /*
+        ------------------------修改模块-----------------------
+     */
+//    /*
+//        修改密码
+//     */
+//    public int updatePassword(Integer id,String password,String repassword){
+//        int i = service.updatePassword(id, password, repassword);
+//        return i;
+//    }
+
+    /*
+        修改手机号
+     */
+
+
+}
