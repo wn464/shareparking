2f73387db0866f1117bcb16097e4d5f1812befc9
zhouchengzhuang <97674657@qq.com>
2019-07-24 15:34:50 +0800

    asdsadasd

--- SharedParking/ShareParking-user-8001/src/main/java/com/project/demo/Application8001Demo.java
+++ SharedParking/ShareParking-user-8001/src/main/java/com/project/demo/Application8001Demo.java
@@ -3,6 +3,7 @@ package com.project.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("com.project.shiro")
 @ComponentScan("com.project.util")
 @MapperScan("com.project.dao")
+@EnableEurekaClient
 public class Application8001Demo {
     public static void main(String[] args) {
         SpringApplication.run(Application8001Demo.class,args);
