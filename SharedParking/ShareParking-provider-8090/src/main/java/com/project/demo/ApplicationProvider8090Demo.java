309880bc9e0711c588edf6ebe7c050f565f446dc
zhangbo314159 <zhangbo_ss@126.com>
2019-07-24 14:37:00 +0800

    1

--- SharedParking/ShareParking-provider-8090/src/main/java/com/project/demo/ApplicationProvider8090Demo.java
+++ SharedParking/ShareParking-provider-8090/src/main/java/com/project/demo/ApplicationProvider8090Demo.java
@@ -3,13 +3,12 @@ package com.project.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@EnableEurekaClient
-@MapperScan("com.project.dao")
-@ComponentScan({"com.project.controller","com.project.config","com.project.service"})
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class ApplicationProvider8090Demo {
 
 	public static void main(String[] args) {
