77938cfcd333f09aab03100d342155bf87465bb9
zhaoxuechuan <2238421926@qq.com>
2019-07-25 12:21:53 +0800

    sdfadf

--- SharedParking/ShareParking-comment/src/main/java/com/project/Bean/CredibilityBean.java
+++ SharedParking/ShareParking-comment/src/main/java/com/project/Bean/CredibilityBean.java
@@ -1,11 +1,13 @@
 package com.project.Bean;
 
+import java.io.Serializable;
+
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 @Data
 @Accessors(chain = true)//链式表达
-public class CredibilityBean {
+public class CredibilityBean implements Serializable{
 	private int id;
 	private MemberBean m_id;
 	private int ordersum;
