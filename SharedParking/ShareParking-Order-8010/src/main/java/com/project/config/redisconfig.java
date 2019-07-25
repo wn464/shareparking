fd10177da49945143b337858777701b223cf325a
zhangbo314159 <zhangbo_ss@126.com>
2019-07-25 20:33:18 +0800

    首页和财务统计

---
+++ SharedParking/ShareParking-Order-8010/src/main/java/com/project/config/redisconfig.java
@@ -0,0 +1,37 @@
+package com.project.config;
+
+import java.lang.reflect.Method;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置redis类，
+ * 配置key的生成策略
+ *开启缓存
+ *默认使用类的权限定类名来作为key
+ *
+ */
+@Configuration
+@EnableCaching //开启缓存
+public class redisconfig extends CachingConfigurerSupport {
+@Override
+@Bean
+public KeyGenerator keyGenerator() {
+	
+	return new KeyGenerator() {
+		@Override
+		public Object generate(Object target, Method method, Object... params) {
+			StringBuilder sb=new StringBuilder();
+			//获取类类名
+			sb.append(target.getClass().getName());
+			//获取方法名
+			sb.append(method.getName());
+			return sb.toString();
+		}
+	};
+}
+}
