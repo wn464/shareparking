cc9c17238d0ae153e362dca5186fdd206f1f7bf3
zhaoxuechuan <2238421926@qq.com>
2019-07-27 17:45:18 +0800

    wertwert

--- SharedParking/ShareParking_carport_8050/src/main/java/com/project/Serviceimpl/Carportserviceimpl.java
+++ SharedParking/ShareParking_carport_8050/src/main/java/com/project/Serviceimpl/Carportserviceimpl.java
@@ -3,7 +3,6 @@ package com.project.Serviceimpl;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
@@ -14,6 +13,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.project.Bean.CarportBean;
+import com.project.Bean.ImagesBean;
 import com.project.Bean.MarkBean;
 import com.project.Bean.PageBean;
 import com.project.IService.CarportIService;
@@ -158,7 +158,10 @@ private CarportDao dao;
 	@Override
 	@Transactional(isolation = Isolation.DEFAULT,propagation = Propagation.REQUIRED)
 	public boolean addcarport(CarportBean carport) {
-		idao.addimages(carport.getImgs_id());
+		int v=idao.addimages(carport.getImgs_id());
+		ImagesBean m=new ImagesBean();
+		m.setId(v);
+		carport.setImgs_id(m);
 		int s=dao.addcarport(carport);
 		if(s>0) {
 			return true;
