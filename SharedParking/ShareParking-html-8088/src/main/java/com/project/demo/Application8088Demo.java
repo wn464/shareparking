705995bed88ba683efb03e92c85aaa224e36e05c
zhouchengzhuang <97674657@qq.com>
2019-07-30 18:31:57 +0800

    123

--- SharedParking/ShareParking-html-8088/src/main/java/com/project/demo/Application8088Demo.java
+++ SharedParking/ShareParking-html-8088/src/main/java/com/project/demo/Application8088Demo.java
@@ -3,8 +3,9 @@ package com.project.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-
+@EnableRedisHttpSession
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class Application8088Demo {
 
