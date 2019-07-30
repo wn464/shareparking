62201a3e3a0beea3f5599e58aef5824270d407be
zhaoxuechuan <2238421926@qq.com>
2019-07-30 16:22:27 +0800

    sdfa

---
+++ SharedParking/ShareParking-comment/src/main/java/com/project/dao/CarDao.java
@@ -0,0 +1,35 @@
+package com.project.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.One;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import com.project.Bean.CarBean;
+
+public interface CarDao {
+	/**
+	 * 添加车位
+	 * @param car
+	 * @return
+	 */
+	@Insert("insert into car (m_id,carnumber) values(#{car.m_id.id},#{car.carnumber})")
+int addcar(@Param("car")CarBean car);
+/**
+ * 查询车位
+ * @param memid
+ * @return
+ */
+	@Select("select * from car where m_id=#{memid}")
+	@Results({
+			@Result(property = "id",column = "id"),
+			@Result(property="m_id",column = "m-id",one=@One(select="com.project.dao.IMemberDao.findById")),
+			@Result(property="carnumber",column = "carnumber")
+	})
+		
+List<CarBean> findcarbymid(int memid);
+}
