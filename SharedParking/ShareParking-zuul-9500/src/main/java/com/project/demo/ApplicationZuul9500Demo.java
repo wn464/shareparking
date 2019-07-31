5d82757dbaebffcf4e14032ded0f42df7514fe71
LDnanchao <1393767576@qq.com>
2019-07-31 11:08:21 +0800

    sss

--- SharedParking/ShareParking-zuul-9500/src/main/java/com/project/demo/ApplicationZuul9500Demo.java
+++ SharedParking/ShareParking-zuul-9500/src/main/java/com/project/demo/ApplicationZuul9500Demo.java
@@ -1,6 +1,5 @@
 package com.project.demo;
 
-import javax.servlet.http.HttpSession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaClient
@@ -22,9 +20,5 @@ public class ApplicationZuul9500Demo {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationZuul9500Demo.class, args);
-	}
-	@GetMapping("/zuul/sessionid")
-	public String sessionid(HttpSession session) {
-		return session.getId().toString();
 	}
 }
--- SharedParking/ShareParking-zuul-9500/src/main/java/com/project/sso/filter/AuthFilter.java
+++ SharedParking/ShareParking-zuul-9500/src/main/java/com/project/sso/filter/AuthFilter.java
@@ -47,13 +47,27 @@ public class AuthFilter extends ZuulFilter{
       //CORS实现跨域
       //https://www.cnblogs.com/lazyInsects/p/8110758.html
       HttpServletResponse res =response;  
+      String origin = request.getHeader("Origin");
+      if(origin == null) {
+          origin = request.getHeader("Referer");
+      }
 	  res.setContentType("text/html;charset=UTF-8");  
-	  res.setHeader("Access-Control-Allow-Origin", "*");  
+	  res.setHeader("Access-Control-Allow-Origin", origin);  
 	  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");  
-	  res.setHeader("Access-Control-Max-Age", "0");  
+	  res.setHeader("Access-Control-Max-Age", "30");  
 	  res.setHeader("Access-Control-Allow-Headers", "Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With,userId,token");  
 	  res.setHeader("Access-Control-Allow-Credentials", "true");  
 	  res.setHeader("XDomainRequestAllowed","1");  
+	  Cookie[] cookies = request.getCookies();
+      if(null != cookies){
+          for (Cookie cookie : cookies) {
+              System.out.println(cookie.getName()+":"+cookie.getValue());
+          }
+      }
+      if(cookies==null) {
+    	  System.out.println("cookie为空");
+      }
+      
 	  return false;
 	}
 
