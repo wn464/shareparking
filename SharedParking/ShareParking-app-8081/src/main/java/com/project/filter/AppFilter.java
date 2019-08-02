040eb7088dae789fd76c3ff53cb60279f4822ba1
LDnanchao <1393767576@qq.com>
2019-08-02 12:18:21 +0800

    fff

--- SharedParking/ShareParking-app-8081/src/main/java/com/project/filter/AppFilter.java
+++ SharedParking/ShareParking-app-8081/src/main/java/com/project/filter/AppFilter.java
@@ -46,6 +46,18 @@ public class AppFilter implements Filter {
 		        HttpServletResponse resp = (HttpServletResponse) response;
 		        //访问路径
 		 
+		        
+		      //resp.setHeader:发送一个报头，告诉浏览器当前页面不进行缓存，每次访问的时间必须从服务器上读取最新的数据
+				//no-cache：客户端每次请求时必须向服务器发送
+				//must-revalidate:作用与no-cache相同，但更严谨
+				//no-store:缓存将不存在response,包括header和body。
+				resp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+				// 本地无缓存，自动刷新页面
+				resp.setHeader("Pragma", "no-cache"); 
+				// Expires实体报头域给出响应过期的日期和时间，小于等于0表示当前页面立即过期，
+				// 为了让浏览器不要缓存页面，也可以利用Expires实体报关域，设置为0
+				resp.setDateHeader("Expires", 0); 
+				
 		        String url = req.getRequestURL().toString();
 		        String uri = req.getRequestURI().toString();
 		        System.out.println(url);
